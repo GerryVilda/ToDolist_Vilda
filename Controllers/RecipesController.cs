3a76e7ed45730826326c143f5509fe28743c95ca
GerryCV1007 <gerrycv10@gmail.com>
2025-09-20 15:52:44 +0800

    ...

--- Controllers/RecipesController.cs
+++ Controllers/RecipesController.cs
@@ -40,15 +40,36 @@ namespace ToDolist_Vilda.Controllers
         {
             if (ModelState.IsValid)
             {
-                // Create new title entry from helper property
-                var title = new RecipeTitle { TitleName = recipe.TitleName };
-                recipe.RecipeTitle = title;
+                // Check if a RecipeTitle with the same name already exists
+                var existingTitle = await _context.RecipeTitles
+                                                  .FirstOrDefaultAsync(t => t.TitleName == recipe.TitleName);
 
-                _context.Add(recipe);
+                if (existingTitle != null)
+                {
+                    // Use the existing title
+                    recipe.RecipeTitleId = existingTitle.Id; // Set FK
+                }
+                else
+                {
+                    // Create a new title
+                    var newTitle = new RecipeTitle { TitleName = recipe.TitleName };
+                    _context.RecipeTitles.Add(newTitle);
+                    await _context.SaveChangesAsync(); // Save first to get the ID
+
+                    recipe.RecipeTitleId = newTitle.Id; // Set FK
+                }
+
+                // Add the recipe itself to the context
+                _context.Recipes.Add(recipe);
+
+                // Save changes
                 await _context.SaveChangesAsync();
+
                 return RedirectToAction(nameof(Index));
             }
+
             return View(recipe);
         }
+
     }
 }
--- Data/RecipeTitle.cs
+++ Data/RecipeTitle.cs
@@ -1,15 +1,28 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-
-namespace ToDolist_Vilda.Models
+﻿public class RecipeTitle
 {
-    public class RecipeTitle
-    {
-        public int Id { get; set; }
+    public int Id { get; set; }
+    public string TitleName { get; set; } = string.Empty;
 
-        [Required, StringLength(150)]
-        public string TitleName { get; set; }
+    public ICollection<Recipe> Recipes { get; set; } = new List<Recipe>();
+}
 
-        public virtual ICollection<Recipe> Recipes { get; set; }
-    }
+public class Recipe
+{
+    public int Id { get; set; }
+    public int RecipeTitleId { get; set; }
+    public string Instructions { get; set; } = string.Empty;
+
+    public RecipeTitle RecipeTitle { get; set; }
+    public ICollection<Ingredient> Ingredients { get; set; } = new List<Ingredient>();
+}
+
+public class Ingredient
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string UnitOfMeasure { get; set; } = string.Empty;
+    public double Measure { get; set; }
+    public int RecipeId { get; set; }
+
+    public Recipe Recipe { get; set; }
 }
